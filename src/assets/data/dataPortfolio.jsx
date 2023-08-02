f376eec9b8f23d4a80dec25e597d7a364fd96484
aumnitipath <aumnitipath@outlook.com>
2023-08-03 04:23:14 +0700

    feat:add  picture section portfolio

---
+++ src/assets/data/dataPortfolio.jsx
@@ -0,0 +1,28 @@
+const pictures = [
+  {
+    id: 1,
+    pictureUrl: "/src/assets/images/portfolio-1.png",
+  },
+  {
+    id: 2,
+    pictureUrl: "/src/assets/images/portfolio-2.png",
+  },
+  {
+    id: 3,
+    pictureUrl: "/src/assets/images/portfolio-3.png",
+  },
+  {
+    id: 4,
+    pictureUrl: "/src/assets/images/portfolio-4.png",
+  },
+  {
+    id: 5,
+    pictureUrl: "/src/assets/images/portfolio-5.png",
+  },
+  {
+    id: 6,
+    pictureUrl: "/src/assets/images/portfolio-6.png",
+  },
+];
+
+export default pictures;
--- src/component/Portfolio.jsx
+++ src/component/Portfolio.jsx
@@ -1,8 +1,28 @@
-import React from "react";
 import "./Portfolio.css";
+import pictures from "../assets/data/dataPortfolio";
 
 function Portfolio() {
-  return <div className="portfolio-container"></div>;
+  return (
+    <div className="portfolio-container">
+      <div className="portfolio-header">
+        <div className="portfolio-text-head">
+          <h1>Portfolio</h1>
+        </div>
+        <div className="portfolio-text-more">
+          See More <span>→</span>
+        </div>
+      </div>
+      <div className="portfolio-box">
+        {pictures.map((item) => {
+          return (
+            <div key={item.id} className="portfolio-grid">
+              <img src={item.pictureUrl} />
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
 }
 
 export default Portfolio;
