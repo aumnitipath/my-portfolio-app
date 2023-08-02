b28a796328454ffac4797c1e2b047a292d6b2053
aumnitipath <aumnitipath@outlook.com>
2023-08-03 01:47:37 +0700

    feat:build section portfolio

--- src/component/Portfolio.jsx
+++ src/component/Portfolio.jsx
@@ -1,7 +1,8 @@
 import React from "react";
+import "./Portfolio.css";
 
 function Portfolio() {
-  return <div>Portfolio</div>;
+  return <div className="portfolio-container"></div>;
 }
 
 export default Portfolio;
