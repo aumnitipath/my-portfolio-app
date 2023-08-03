10ace441bd481b9b4c232a8597093bc7c32a104c
aumnitipath <aumnitipath@outlook.com>
2023-08-03 17:40:42 +0700

    fix:run build missing picture

--- src/component/Portfolio.jsx
+++ src/component/Portfolio.jsx
@@ -1,5 +1,10 @@
 import "./Portfolio.css";
-import pictures from "../assets/data/dataPortfolio";
+import pictures1 from "/src/assets/images/portfolio-1.png";
+import pictures2 from "/src/assets/images/portfolio-2.png";
+import pictures3 from "/src/assets/images/portfolio-3.png";
+import pictures4 from "/src/assets/images/portfolio-4.png";
+import pictures5 from "/src/assets/images/portfolio-5.png";
+import pictures6 from "/src/assets/images/portfolio-6.png";
 
 function Portfolio() {
   return (
@@ -13,13 +18,24 @@ function Portfolio() {
         </div>
       </div>
       <div className="portfolio-box">
-        {pictures.map((item) => {
-          return (
-            <div key={item.id} className="portfolio-grid">
-              <img src={item.pictureUrl} />
-            </div>
-          );
-        })}
+        <div className="portfolio-grid">
+          <img src={pictures1} />
+        </div>
+        <div className="portfolio-grid">
+          <img src={pictures2} />
+        </div>
+        <div className="portfolio-grid">
+          <img src={pictures3} />
+        </div>
+        <div className="portfolio-grid">
+          <img src={pictures4} />
+        </div>
+        <div className="portfolio-grid">
+          <img src={pictures5} />
+        </div>
+        <div className="portfolio-grid">
+          <img src={pictures6} />
+        </div>
       </div>
     </div>
   );
